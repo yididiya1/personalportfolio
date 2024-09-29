07944092ff3b56ee1e0ff2f054a93f090fa45df7
yididiya1 <yididiyakebede@gmail.com>
2024-09-29 12:27:10 +0300

    Hero page color and UI update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,29 +2,47 @@ import React from 'react'
 import { CiLinkedin } from 'react-icons/ci'
 import { FaBehance, FaDribbble, FaGithub } from 'react-icons/fa'
 
+// E63929
+// C44BF1
+
 const Hero = () => {
   return (
-    <div className='max-w-[1440px] mx-auto h-[90vh] flex px-[9vw] items-center justify-center'>
-        <div className='flex-1 flex space-x-14 justify-center items-center'>
-            <div className='flex flex-col items-center'>
-                <div className='h-24 border-r-2 border-[#92FE00]'></div>
-                <div className='my-10 space-y-6 flex flex-col items-center'>
-                    <CiLinkedin size={35} color='#92FE00'/>
-                    <FaDribbble size={25} color='#92FE00' />
-                    <FaBehance size={25} color='#92FE00' />
-                    <FaGithub size={25} color='#92FE00' />
-                </div>
-                <div className='h-24 border-r-2 border-[#92FE00]'></div>
-            </div>
-            <div className=''>
-            <p className='text-[2.8vw]'>👋 Hello, I&apos;m <span className='text-[#92FE00]'>Yididiya Kebede</span></p>
-            <p className='text-[5.3vw] text-[#92FE00] font-bold py-0 leading-tight mb-4'>UI/UX Designer</p>
-            <p className='font-light w-[95%] leading-7'>I’m Iryna, a UI/UX Designer based in Chicago, IL, originally from Ukraine. My background in education and social work, along with living in different countries, fuels my love for understanding diverse perspectives. Transitioning to UI/UX design just felt right—I’ve always put users first. My intuitive designs aim for seamless interactions. I’m all about collaboration, working with cross-functional teams to innovate solutions for positive change</p>
-            </div>  
+    <div className='max-w-[1440px] mx-auto h-[90vh] flex px-[8vw] items-center justify-center'>
+      <div className='flex-1 flex space-x-14 justify-center items-center'>
+        {/* Social Media Icons */}
+        <div className='flex flex-col items-center'>
+          <div className='h-32 w-[2px] bg-gradient-to-b from-[#E63929] to-[#C44BF1] rounded-full'></div>
+          <svg width="0" height="0">
+            <linearGradient id="blue-gradient" x1="100%" y1="100%" x2="0%" y2="0%">
+              <stop stopColor="#E63929" offset="40%" />
+              <stop stopColor="#C44BF1" offset="100%" />
+            </linearGradient>
+          </svg>
+          <div className='my-10 space-y-6 flex flex-col items-center'>
+            <CiLinkedin size={40} style={{ fill: "url(#blue-gradient)" }} />
+            <FaDribbble size={32} style={{ fill: "url(#blue-gradient)" }} />
+            <FaBehance size={30} style={{ fill: "url(#blue-gradient)" }} />
+            <FaGithub size={30} style={{ fill: "url(#blue-gradient)" }}/>
+          </div>
+          <div className='h-32 w-[2px] bg-gradient-to-b from-[#E63929] to-[#C44BF1] rounded-full'></div>
         </div>
-        <div className='flex-1'>
+        {/* Social Media Icons */}
 
+        {/* Title and Description and CTA */}
+        <div className=''>
+          <p className='text-[4vw] leading-[5vw]'>👋 Hello, I&apos;m <span className='bg-gradient-to-r text-transparent from-[#E63929] to-[#C44BF1] bg-clip-text'>Yididiya Kebede, a </span><span className='text-[4vw] bg-gradient-to-r text-transparent from-[#E63929] to-[#C44BF1] bg-clip-text font-bold py-0 leading-tight mb-4'>UI/UX Designer.</span></p>
+          <p className='font-light w-[95%] leading-7'>Innovative UI/UX Designer with a strong background in user research, wireframing, prototyping, and usability testing. Proficient in Figma, Adobe XD, and Sketch, with a proven track record of enhancing user engagement and satisfaction through intuitive, user-centric designs.</p>
+          <div className='flex space-x-3 mt-6'>
+            <button className='bg-gradient-to-r from-[#E63929] to-[#C44BF1] rounded-full py-3 px-6 text-lg'>Let's Connect</button>
+            <button className='bg-[#C44BF1] rounded-full py-3 px-6 text-lg'>Check My Works</button>
+          </div>
         </div>
+        {/* Title and Description */}
+
+      </div>
+      <div className='flex-1'>
+
+      </div>
     </div>
   )
 }
